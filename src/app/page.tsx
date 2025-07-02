035d4257f2a90877587f0bef728f28c7bb324591
piotr.swierzy <piotr.swierzy@arianelabs.com>
2025-07-02 11:39:29 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,26 @@
 "use client"
 
-import {ChatMessage} from "@/shared/types";
-import {useState} from "react";
-import {useHandleChat} from "@/api/handle-chat";
+import { ChatMessage } from "@/shared/types";
+import { useState } from "react";
+import { useHandleChat } from "@/api/handle-chat";
 import { Transaction } from '@hashgraph/sdk';
-import {ChatInput} from "@/components/chat-input";
-import {EmptyChat} from "@/components/empty-chat";
-import {Header} from "@/components/header";
-import {useAppKitProvider, useAppKitState} from "@reown/appkit/react";
-import {ChainNamespace} from "@reown/appkit-common";
-import {HederaProvider, transactionToBase64String} from "@hashgraph/hedera-wallet-connect";
+import { ChatInput } from "@/components/chat-input";
+import { EmptyChat } from "@/components/empty-chat";
+import { Header } from "@/components/header";
+import { useAppKitProvider, useAppKitState } from "@reown/appkit/react";
+import { ChainNamespace } from "@reown/appkit-common";
+import { HederaProvider, transactionToBase64String } from "@hashgraph/hedera-wallet-connect";
 
 export default function Home() {
     const [chatHistory, setChatHistory] = useState<ChatMessage[]>([])
     const [prompt, setPrompt] = useState("");
-    const {mutateAsync, isPending} = useHandleChat();
+    const { mutateAsync, isPending } = useHandleChat();
 
     const { activeChain } = useAppKitState()
-    const { walletProvider } = useAppKitProvider(activeChain ?? ('hedera' as ChainNamespace))
+    const { walletProvider } = useAppKitProvider('hedera' as ChainNamespace);
+
+    console.log('activeChain:', activeChain);
+    console.log('walletProvider:', walletProvider);
 
     const getWalletProvider = () => {
         if (!walletProvider) throw Error('user is disconnected')
@@ -39,7 +42,7 @@ export default function Home() {
             transactionList: transactionToBase64String(transaction),
         })
 
-        console.log({signTransactionResponse})
+        console.log({ signTransactionResponse })
     }
 
     async function handleUserMessage() {
@@ -60,45 +63,45 @@ export default function Home() {
             content: agentResponse.message
         }])
 
-        if(agentResponse.transactionBytes) {
+        if (agentResponse.transactionBytes) {
             const txBytes = Buffer.from(agentResponse.transactionBytes, 'base64');
             const transaction = Transaction.fromBytes(txBytes);
-            console.log({transaction})
+            console.log({ transaction })
         }
 
         console.log(prompt)
     }
 
-  return (
-    <div className="h-screen w-full bg-zinc-900 flex items-center justify-center flex-col">
-        <main className="w-4xl h-full flex flex-col">
-            <Header />
-            <button onClick={signExampleTransaction}>
-                essen
-            </button>
-            <div className="bg-zinc-800 grow rounded-lg flex flex-col gap-2 p-4">
-                {
-                    chatHistory.map((message, idx) => (
-                        <div key={idx} className="flex">
-                            {
-                                message.type === "human" ? (
-                                    <div className="bg-zinc-700 inline-block px-4 py-2 rounded-md ml-auto">
-                                        {message.content}
-                                    </div>
-                                ) : (
-                                    <div className="bg-zinc-700 inline-block px-4 py-2 rounded-md break-all">
-                                        {message.content}
-                                    </div>
-                                )
-                            }
-                        </div>
-                    ))
-                }
+    return (
+        <div className="h-screen w-full bg-zinc-900 flex items-center justify-center flex-col">
+            <main className="w-4xl h-full flex flex-col">
+                <Header />
+                <button onClick={signExampleTransaction}>
+                    essen
+                </button>
+                <div className="bg-zinc-800 grow rounded-lg flex flex-col gap-2 p-4">
+                    {
+                        chatHistory.map((message, idx) => (
+                            <div key={idx} className="flex">
+                                {
+                                    message.type === "human" ? (
+                                        <div className="bg-zinc-700 inline-block px-4 py-2 rounded-md ml-auto">
+                                            {message.content}
+                                        </div>
+                                    ) : (
+                                        <div className="bg-zinc-700 inline-block px-4 py-2 rounded-md break-all">
+                                            {message.content}
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        ))
+                    }
 
-                <EmptyChat isChatEmpty={chatHistory.length <= 0}/>
-            </div>
-            <ChatInput handleUserMessage={handleUserMessage} prompt={prompt} setPrompt={setPrompt} isPending={isPending}/>
-        </main>
-    </div>
-  );
+                    <EmptyChat isChatEmpty={chatHistory.length <= 0} />
+                </div>
+                <ChatInput handleUserMessage={handleUserMessage} prompt={prompt} setPrompt={setPrompt} isPending={isPending} />
+            </main>
+        </div>
+    );
 }
--- src/components/client-providers.tsx
+++ src/components/client-providers.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import {ReactNode} from "react";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {hederaTestnet} from "@reown/appkit/networks";
-import {createAppKit} from "@reown/appkit/react";
+import { ReactNode, useEffect, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { hederaTestnet } from "@reown/appkit/networks";
+import { createAppKit } from "@reown/appkit/react";
 import {
     HederaAdapter, HederaChainDefinition, hederaNamespace, HederaProvider,
 } from "@hashgraph/hedera-wallet-connect";
@@ -13,53 +13,63 @@ const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID ?? "";
 
 const queryClient = new QueryClient()
 
-if(typeof window !== "undefined") {
-    const metadata = {
-        name: 'AgentKit Next.js Demo',
-        description: 'AgentKit Next.js Demo',
-        url: 'https://example.com',
-        icons: ['https://avatars.githubusercontent.com/u/179229932']
-    }
+const metadata = {
+    name: 'AgentKit Next.js Demo',
+    description: 'AgentKit Next.js Demo',
+    url: 'https://example.com',
+    icons: ['https://avatars.githubusercontent.com/u/179229932']
+}
 
-    const hederaEVMAdapter = new HederaAdapter({
-        projectId,
-        networks: [
-            HederaChainDefinition.EVM.Mainnet,
-            HederaChainDefinition.EVM.Testnet,
-        ],
-        namespace: 'eip155',
-    })
+const hederaNativeAdapter = new HederaAdapter({
+    projectId,
+    networks: [HederaChainDefinition.Native.Mainnet, HederaChainDefinition.Native.Testnet],
+    namespace: hederaNamespace, // 'hedera'
+})
 
-    const hederaNativeAdapter = new HederaAdapter({
-        projectId,
-        networks: [HederaChainDefinition.Native.Mainnet, HederaChainDefinition.Native.Testnet],
-        namespace: hederaNamespace, // 'hedera'
-    })
+export function ClientProviders({ children }: ClientProvidersProps) {
+    const [isReady, setIsReady] = useState(false);
 
-    const universalProvider = (await HederaProvider.init({
-        projectId,
-        metadata,
-    })) as unknown as UniversalProvider // avoid type mismatch error due to missing of private properties in HederaProvider
+    useEffect(() => {
+        // Remove any persisted chain selection
+        localStorage.removeItem('appkit.activeChain');
+        sessionStorage.removeItem('appkit.activeChain');
+    }, []);
 
-    createAppKit({
-        adapters: [hederaEVMAdapter, hederaNativeAdapter],
-        //@ts-expect-error expected type error
-        universalProvider,
-        networks: [hederaTestnet],
-        projectId,
-        metadata,
-        defaultNetwork: hederaTestnet
-    })
+    useEffect(() => {
+        let isMounted = true;
+        async function init() {
+            const universalProvider = await HederaProvider.init({
+                projectId,
+                metadata,
+            }) as unknown as UniversalProvider;
+
+            console.log('hederaNativeAdapter:', hederaNativeAdapter);
+            console.log('universalProvider:', universalProvider);
+
+            createAppKit({
+                adapters: [hederaNativeAdapter],
+                // @ts-expect-error expected type error
+                universalProvider,
+                networks: [hederaTestnet],
+                projectId,
+                metadata,
+                defaultNetwork: hederaTestnet
+            });
+            if (isMounted) setIsReady(true);
+        }
+        init();
+        return () => { isMounted = false; };
+    }, []);
+
+    if (!isReady) return <div style={{ color: 'white', textAlign: 'center', marginTop: '2rem' }}>Loading wallet...</div>;
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
 }
 
 type ClientProvidersProps = {
-    children: ReactNode
-}
-
-export function ClientProviders({children}: ClientProvidersProps) {
-    return (
-            <QueryClientProvider client={queryClient}>
-                {children}
-            </QueryClientProvider>
-    )
+    children: React.ReactNode
 }
